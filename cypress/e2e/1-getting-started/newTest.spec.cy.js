d02f1a139e46bf8402052a7adc9eb73980e17583
bharath9856 <bharathchalla3101@gmail.com>
2023-06-15 21:54:12 +0530

    allure

---
+++ cypress/e2e/1-getting-started/newTest.spec.cy.js
@@ -0,0 +1,27 @@
+
+const User = require("../fixtures/users.json")
+
+describe("User Test Suite", () => {
+    beforeEach(() => {
+      // cy.fixture("users").then((usersData) => {
+      //   const users = usersData.users;
+
+        
+      //   });
+      
+    });
+    User.users.forEach((user,index) => {
+      // `Test case - ${user.username}`;
+
+        it(`testcase ${index}`, function() {
+          cy.log(user.username)
+          cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+          cy.get('#txtUsername').type(user.username);
+          cy.get('#txtPassword').type(user.password);
+          cy.get('#btnLogin').click();
+          // Add more test steps here
+        
+        });
+    
+  });
+});
