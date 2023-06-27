f86d8437428699b978eb0ef34083c3ae949ecfb2
bharath9856 <bharathchalla3101@gmail.com>
2023-06-27 18:56:09 +0530

    delete test case

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -98,7 +98,7 @@ describe('example to-do app', () => {
 
       // After filtering, we can assert that there is only the one
       // incomplete item in the list.
-      cy.get('.todo-lit li')
+      cy.get('.todo-list li')
         .should('have.length', 1)
         .first()
         .should('have.text', 'Walk the dog')
